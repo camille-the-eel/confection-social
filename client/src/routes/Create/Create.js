05eba9133192c1c8006fa74325f1996589712d55
Camille <c.hughes64@gmail.com>
2019-04-23 19:55:44 -0400

    create component closeButton (non-functional)

--- client/src/routes/Create/Create.js
+++ client/src/routes/Create/Create.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import closeButton from '../../img/closeButton.svg';
 import './style.css';
 
 class Create extends Component {
@@ -14,24 +15,29 @@ class Create extends Component {
     render () {
         return (
             <div id="divBody">
-                <div className="photoDiv">
-                    <p id="photoHead">photo</p>
-                    <div id="upload">
+                <div id="padding">
+                    <div className="photoDiv">
+                        <p id="photoHead">
+                            photo
+                            <img src={closeButton} alt="closeButton" id="close" onClick=""/>
+                        </p>
+                        <div id="upload">
+                        </div>
                     </div>
+                    <div className="text-input">
+                        <p id="captionHead">caption</p>
+                        <textarea className="commentInput"/>
+                    </div>
+                    <div className="input-field">
+                        <p id="creditHead">credit</p>
+                        <input id="credit" type="text" className="validate addition"/>
+                    </div>
+                    <div className="input-field">
+                        <p id="tagsHead">tags</p>
+                        <input id="tags" type="text" className="validate addition"/>
+                    </div>
+                    <button className="postButton">post</button>
                 </div>
-                <div className="text-input">
-                    <p id="captionHead">caption</p>
-                    <textarea className="commentInput"/>
-                </div>
-                <div className="input-field">
-                    <p id="creditHead">credit</p>
-                    <input id="credit" type="text" className="validate addition"/>
-                </div>
-                <div className="input-field">
-                    <p id="tagsHead">tags</p>
-                    <input id="tags" type="text" className="validate addition"/>
-                </div>
-                <button className="postButton">post</button>
             </div>
         )
     }
