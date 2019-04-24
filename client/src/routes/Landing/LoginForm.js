36fc1b1defc47f105f4bed6c90cd02288931577c
Camille <c.hughes64@gmail.com>
2019-04-23 23:10:29 -0400

    margins

--- client/src/routes/Landing/LoginForm.js
+++ client/src/routes/Landing/LoginForm.js
@@ -46,6 +46,9 @@ class LoginForm extends Component {
                 <div>
                     <form className="form-horizontal accountForm">
                         <div className="form-group">
+                            <div className="col-1 col-ml-auto">
+                                <label className="form-label" htmlFor="email">Email</label>
+                            </div>
                             <div className="col-3 col-mr-auto">
                                 <input className="form-input"
                                     type="email"
@@ -58,12 +61,14 @@ class LoginForm extends Component {
                             </div>
                         </div>
                         <div className="form-group">
+                            <div className="col-1 col-ml-auto">
+                                <label className="form-label" htmlFor="password">Password</label>
+                            </div>
                             <div className="col-3 col-mr-auto">
                                 <input className="form-input"
                                     type="password"
                                     id="password"
                                     name="password"
-                                    placeholder="password"
                                     value={this.state.password}
                                     onChange={this.handleChange}
                                 />
