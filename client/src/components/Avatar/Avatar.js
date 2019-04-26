36f9f86ff6409cf37095bab3d783dd1c17e1fe94
unknown <dreilingmarshall@gmail.com>
2019-04-26 15:07:38 -0400

    Avatar testing for different parent components

--- client/src/components/Avatar/Avatar.js
+++ client/src/components/Avatar/Avatar.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 // import { Route, Link } from "react-router-dom"
 
+// import API from "../../utils/API";
+
 class Avatar extends Component {
+    state = {
+        image: "",
+        source: "",
+        page_title: "",
+    }
 
     componentDidMount() {
-        console.log(this)
+        console.log(this);
+    }
+
+    // Take props and pass needed info into state
+    fetchPageInfo = (pageID) => {
+        console.log(pageID)
     }
 
     render () {
         return (
             <img
-                src={this.props.children.avatar || "https://via.placeholder.com/250"} 
+                src={this.props.children.avatar || this.fetchPageInfo(this.props.children.source)|| "https://via.placeholder.com/250"} 
                 alt="page-avatar" 
                 style={{width:45}} 
             />
