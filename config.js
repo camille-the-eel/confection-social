700174895f291f974121f073dafd7708b62bfcf8
Camille <c.hughes64@gmail.com>
2019-04-27 17:30:58 -0400

    manifest

--- config.js
+++ config.js
@@ -3,13 +3,6 @@ module.exports = {
     name: 'Confection Social Media App',
     version: '0.0.1',
     env: process.env.NODE_ENV || 'DEVELOPMENT',
-    stream: {
-        appId: process.env.STREAM_APP_ID,
-        key: process.env.STREAM_KEY,
-        secret: process.env.STREAM_SECRET
-    }
-}
-    },
     mongo: {
         username: process.env.MONGO_UN,
         password: process.env.MONGO_PW
