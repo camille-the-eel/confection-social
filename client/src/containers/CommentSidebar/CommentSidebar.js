d4f8cb4864b5a7f392610048c8cda4d7a0fd2903
unknown <dreilingmarshall@gmail.com>
2019-04-27 21:37:18 -0400

    Working post comments

--- client/src/containers/CommentSidebar/CommentSidebar.js
+++ client/src/containers/CommentSidebar/CommentSidebar.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import CommentHeader from "../../components/CommentHeader/CommentHeader";
-import CommentButton from '../../components/CommentButton/CommentButton';
+import CommentCreate from '../../components/CommentCreate/CommentCreate';
 import CommentList from "../../components/CommentList/CommentList";
 import './style.css';
 
 import closeButton from '../../img/closeButton.svg';
+import CurrentUser from '../../AppContext';
 
 class CommentSidebar extends Component {
 
@@ -13,36 +14,31 @@ class CommentSidebar extends Component {
     }
 
     localClick = () => {
-        console.log("local click")
+        console.log("local click");
+        console.log(this);
     }
 
     render () {
         return (
             <div className="fixedDiv">
-            <div className="commentSidebarContainer">
-            <img src={closeButton} alt="closeButton" className="closeButton" onClick={this.props.closeComments}/>
-                <div className="commentSidebar">
-                    <div className="centeredDiv">
-                        <div className="creatorDiv">
-                            <CommentHeader>{this.props.children}</CommentHeader>
-                        </div>
-                        <div className="commentsDiv">
-                           <CommentList>{this.props.children.post_comments}</CommentList>
-                        </div>
-                        <div className="inputCommentDiv">
-                            <textarea className="commentInput"/>
-                            <button className="addComment" onClick={this.localClick}>
-                                <p className="comment">ADD COMMENT</p>
-                                <CommentButton className="commentButton"/>
-                            </button>
+                <div className="commentSidebarContainer">
+                    <img src={closeButton} alt="closeButton" className="closeButton" onClick={this.props.closeComments}/>
+                    <div className="commentSidebar">
+                        <div className="centeredDiv">
+                            <div className="creatorDiv">
+                                <CommentHeader>{this.props.children}</CommentHeader>
+                            </div>
+                            <div className="commentsDiv">
+                                <CommentList>{this.props.children.post_comments}</CommentList>
+                            </div>
+                            <CommentCreate>{this.props.children}</CommentCreate>
                         </div>
                     </div>
                 </div>
             </div>
-            </div>
         )
     }
-
 }
 
+CommentSidebar.contextType = CurrentUser;
 export default CommentSidebar; 
--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -25,6 +25,30 @@ router.post("/create", (req, res) => {
 
 });
 
+// @ POST api/posts/addcomment
+// Adding a comment to a post
+
+router.post("/addcomment", (req, res) => {
+
+    // Update Post where _id = the post id that we are adding to
+    // Using update $push operator to push the new comment into the comments array
+    Post.updateOne(
+        { _id: req.body.post_id},
+        { $push: 
+            { post_comments: 
+                {
+                    comment_text: req.body.comment_text,
+                    comment_author: req.body.comment_author,
+                    cAuthor_id: req.body.cAuthor_id,
+                    cAuthor_Avatar: req.body.cAuthor_avatar
+                }
+            } 
+        }
+    )
+    .then(comment => res.json(comment))
+    .catch(err => res.status(422).json(err));
+});
+
 // @ GET api/posts/home
 // Currently pulls all posts created. Will eventually pull all posts by current users followed blogs
 router.get("/home", (req, res) => {
