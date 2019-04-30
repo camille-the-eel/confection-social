4a239b9ab476e1236a0957e7fd0ba6882409a6f9
Camille <c.hughes64@gmail.com>
2019-04-30 18:25:06 -0400

    rm app.get

--- server.js
+++ server.js
@@ -38,10 +38,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
 
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, "/client/public/index.html"));
-});
-
 app.listen(PORT, function(){
   console.log("listening on: " + PORT);
 });
