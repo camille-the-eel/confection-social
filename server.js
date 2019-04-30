bdf5ca4b166eddab091d18a3f825604caf3023af
Camille <c.hughes64@gmail.com>
2019-04-29 22:01:36 -0400

    app.use line 15

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const routes = require("./server/routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const socket = require('socket.io');
-// const io = socket(server);
+
+app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
 // Bodyparser middleware
 app.use(
@@ -51,22 +51,3 @@ if(process.env.NODE_ENV === 'production'){
 app.get('*',(req, res) => {
   res.sendFile(path.resolve(__dirname, "/client/public/index.html"));
 });
-
-const server = app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
-
-
-// //SOCKET ROUTES
-// //on connection: do these actions
-// io.on('connection', function(socket){
-//   console.log("Socket Connection Successful", socket.id);
-
-//   //listening for emit events from jsx
-//   socket.on('create', function(data){
-    
-//     //send event data to database
-    
-//     //then! 
-//     //send to all other sockets
-//     io.sockets.emit('create', data);
-//   })
-// });
